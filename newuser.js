4ba05dd293c01e328f9be4dc3e24dc799fa61c87
eversley1996 <everboy15@hotmail.com>
2019-11-27 03:57:36 +0000

    Add user page created

---
+++ newuser.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+    loadPage();
+}
+
+let loadPage = () =>{
+    let subButton= document.getElementById("submit_form");
+    
+    button.addEventListener("click", function(){
+        var httpRequest = new XMLHttpRequest();
+        
+        let firstname = document.getElementById("firstname").value.toLowerCase().trim();
+        let lastname = document.getElementById("lastname").value.toLowerCase().trim();
+        let password = document.getElementById("password").valie.trim();
+        let email = document.getElementById("email").value.toLowerCase().trim();
+        
+    });
+}
