b01e14adb46281277b3e45e31000fbfe40136760
eversley1996 <everboy15@hotmail.com>
2019-11-30 23:50:06 -0500

    Added validaiton for new user

--- bugme.php
+++ bugme.php
@@ -1,32 +1,33 @@
 <?php
     session_start();
-    
-    $host = getenv('IP');
-    $username = "eversley";
-    $password = 'E.Francis14';
-    $dbname = 'schema';
-    
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $password = $_POST['password'];
-    $email = $_POST['email'];
-    
-    $firstname = filter_var($firstname, FILTER_SANITIZE_STRING);
-    $lastname = filter_var($lastname, FILTER_SANITIZE_STRING);
-    $password = filter_var($password, FILTER_SANITIZE_STRING);
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-    
-    $password = password_hash($password, PASSWORD_DEFAULT);
-    
-    
-    $_SESSION['firstname'] = $firstname;
-    $_SESSION['lastname'] = $lastname;
-    
-    echo $_SESSION['firstname'], $_SESSION['lastname'];
-    
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    
-    $stmt = $conn->query("INSERT INTO users (`firstname`,`lastname`,`password`,`email`) VALUES 
-    ($firstname, $lastname, $password, $email);");
-    
+    if (isset($_POST['submit_form'])){
+        $host = getenv('IP');
+        $username = "eversley";
+        $password = 'E.Francis14';
+        $dbname = 'schema';
+        
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $password = $_POST['password'];
+        $email = $_POST['email'];
+        
+        $firstname = filter_var($firstname, FILTER_SANITIZE_STRING);
+        $lastname = filter_var($lastname, FILTER_SANITIZE_STRING);
+        $password = filter_var($password, FILTER_SANITIZE_STRING);
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        
+        $password = md5($password);
+        
+        
+        $_SESSION['firstname'] = $firstname;
+        $_SESSION['lastname'] = $lastname;
+        
+        echo $_SESSION['firstname'], $_SESSION['lastname'];
+        
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        
+        $stmt = $conn->query("INSERT INTO users (`firstname`,`lastname`,`password`,`email`) VALUES 
+        ('$firstname', '$lastname', '$password', '$email')");
+
+    } 
 ?>
--- newuser.js
+++ newuser.js
@@ -1,17 +1,63 @@
-window.onload = function(){
-    loadPage();
+let submitForm = (form) =>{
+    let firstname = form.firstname.value;
+    let lastname = form.lastname.value;
+    let password = form.password.value;
+    let email = form.email.value;
+
+    let chkFname = checkFirstName(form, firstname);
+    let chkLname = checkLastName(form, lastname);
+    let chkPass = checkPassword(form, password);
+    let chkEmail = checkEmail(form, email);
+
+    if (chkFname && chkLname && chkPass && chkEmail){
+        return true;
+    }
+    return false;
+}   
+
+let checkPassword= (form, password) =>{
+    let pattern = /(?-i)(?=^.{8,}$)((?!.*\s)(?=.*[A-Z])(?=.*[a-z]))(?=(1)(?=.*\d)|.*[^A-Za-z0-9])^.*$/;
+    
+    if ((password != "") && (pattern.test(password))){
+        return true;
+    }
+    else{
+        form.password.style.color = "red";
+        form.password.style.border = "thick solid red";
+        return false;
+    }
 }
 
-let loadPage = () =>{
-    let subButton= document.getElementById("submit_form");
-    
-    button.addEventListener("click", function(){
-        var httpRequest = new XMLHttpRequest();
-        
-        let firstname = document.getElementById("firstname").value.toLowerCase().trim();
-        let lastname = document.getElementById("lastname").value.toLowerCase().trim();
-        let password = document.getElementById("password").valie.trim();
-        let email = document.getElementById("email").value.toLowerCase().trim();
-        
-    });
+let checkFirstName = (form, firstname) =>{
+    if (firstname == ""){
+        form.firstname.style.color = "red";
+        form.firstname.style.border = "thick solid red";
+        return false;
+    }
+    return true;
+}
+
+let checkLastName = (form, lastname) =>{
+    if (lastname == ""){
+        form.lastname.style.color = "red";
+        form.lastname.style.border = "thick solid red";
+        return false;
+    }
+    return true;
+}
+
+let checkEmail = (form, email) =>{
+    if(email == " " || email == null){
+        form.email.style.color = "red";
+        form.email.style.border = "thick solid red";
+        return false;
+    }else{
+        let pattern =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if((pattern.test(email)) == false){
+            form.email.style.color = "red";
+            form.email.style.border = "thick solid red";
+            return false;
+        }
+        return true;
+    }
 }
